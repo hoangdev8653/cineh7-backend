065d8a24f8f5145ee32aee86c74d0cc5ebd5cd74
hhoang777 <hhoang1072003@gmail.com>
2026-02-25 17:47:46 +0700

    feat(setup): update infrastructure, dependencies and common utils

---
+++ apps/configs/corsConfig.ts
@@ -0,0 +1,18 @@
+import { allowedOrigins } from '../utils/allowedOrigins';
+
+export const corsConfig = {
+  origin: (
+    origin: string,
+    callback: (error: Error | null, allow?: boolean) => void,
+  ) => {
+    if (!origin) return callback(null, true);
+
+    if (allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      console.warn(`Blocked CORS for origin: ${origin}`);
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  credentials: true,
+};
---
+++ apps/utils/allowedOrigins.ts
@@ -0,0 +1,5 @@
+export const allowedOrigins = [
+  'http://localhost:5173',
+  'https://your-production-app.com',
+  'https://admin-dashboard.com',
+];
