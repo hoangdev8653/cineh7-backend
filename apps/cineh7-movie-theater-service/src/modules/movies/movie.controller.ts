b31ef9ddf997e37c38166576e98c3d08eba884aa
hhoang777 <hhoang1072003@gmail.com>
2026-02-25 17:49:08 +0700

    feat(movie): update movie controller in theater service

--- apps/cineh7-movie-theater-service/src/modules/movies/movie.controller.ts
+++ apps/cineh7-movie-theater-service/src/modules/movies/movie.controller.ts
@@ -5,50 +5,64 @@ import { MessagePattern, Payload } from '@nestjs/microservices';
 
 @Controller()
 export class MovieController {
-    constructor(private readonly movieService: MovieService) { }
+  constructor(private readonly movieService: MovieService) {}
 
-    @MessagePattern({ cmd: MOVIE_CMD.CREATE })
-    async createMovie(@Payload() data: { createMovieDto: CreateMovieDto, file: Express.Multer.File }) {
-        const movie = await this.movieService.createMovie(data.createMovieDto, data.file);
-        return {
-            message: 'Tạo phim mới thành công',
-            movie,
-        }
-    }
+  @MessagePattern({ cmd: MOVIE_CMD.CREATE })
+  async createMovie(
+    @Payload()
+    data: {
+      createMovieDto: CreateMovieDto;
+      file: Express.Multer.File;
+    },
+  ) {
+    const movie = await this.movieService.createMovie(
+      data.createMovieDto,
+      data.file,
+    );
+    return {
+      message: 'Tạo phim mới thành công',
+      data: movie,
+    };
+  }
 
-    @MessagePattern({ cmd: MOVIE_CMD.GET_ALL })
-    async getAllMovies() {
-        const movies = await this.movieService.getAllMovies();
-        return {
-            message: 'Lấy danh sách phim thành công',
-            movies,
-        }
-    }
+  @MessagePattern({ cmd: MOVIE_CMD.GET_ALL })
+  async getAllMovies() {
+    const movies = await this.movieService.getAllMovies();
+    return {
+      message: 'Lấy danh sách phim thành công',
+      data: movies,
+    };
+  }
 
-    @MessagePattern({ cmd: MOVIE_CMD.GET_BY_ID })
-    async getMovieById(@Payload() id: string) {
-        const movie = await this.movieService.getMovieById(id);
-        return {
-            message: 'Lấy thông tin phim thành công',
-            movie,
-        }
-    }
+  @MessagePattern({ cmd: MOVIE_CMD.GET_BY_ID })
+  async getMovieById(@Payload() id: string) {
+    const movie = await this.movieService.getMovieById(id);
+    return {
+      message: 'Lấy thông tin phim thành công',
+      data: movie,
+    };
+  }
 
-    @MessagePattern({ cmd: MOVIE_CMD.UPDATE })
-    async updateMovieById(@Payload() data: { id: string, updateMovieDto: UpdateMovieDto }) {
-        const movie = await this.movieService.updateMovieById(data.id, data.updateMovieDto);
-        return {
-            message: 'Cập nhật thông tin phim thành công',
-            movie,
-        }
-    }
+  @MessagePattern({ cmd: MOVIE_CMD.UPDATE })
+  async updateMovieById(
+    @Payload() data: { id: string; updateMovieDto: UpdateMovieDto },
+  ) {
+    const movie = await this.movieService.updateMovieById(
+      data.id,
+      data.updateMovieDto,
+    );
+    return {
+      message: 'Cập nhật thông tin phim thành công',
+      data: movie,
+    };
+  }
 
-    @MessagePattern({ cmd: MOVIE_CMD.DELETE })
-    async deleteMovieById(@Payload() id: string) {
-        const movieDeleted = await this.movieService.deleteMovieById(id);
-        return {
-            message: 'Xóa phim thành công',
-            movieDeleted,
-        }
-    }
+  @MessagePattern({ cmd: MOVIE_CMD.DELETE })
+  async deleteMovieById(@Payload() id: string) {
+    const movieDeleted = await this.movieService.deleteMovieById(id);
+    return {
+      message: 'Xóa phim thành công',
+      data: movieDeleted,
+    };
+  }
 }
